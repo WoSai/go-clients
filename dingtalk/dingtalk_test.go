19fb52544dc726c251b1ead0e15884ea47935f1f
Richard1ybb <yangtianyangxu@qq.com>
2020-12-01 13:57:37 +0800

    test workflow5

--- dingtalk/dingtalk_test.go
+++ dingtalk/dingtalk_test.go
@@ -29,6 +29,23 @@ func init()  {
 	}
 }
 
+func TestClient_WithAppOption(t *testing.T) {
+	opt := Option{AgentID: "AgentID", AppKey: "AppKey", AppSecret: "AppSecret"}
+	DingClient.WithAppOption(opt)
+	assert.Equal(t, DingClient.opt, opt)
+}
+
+func TestClient_GetUserInfoByCode(t *testing.T) {
+	_, _, err := DingClient.GetUserInfoByCode(ctx, &RequestGetUserInfoByCode{TempAuthCode: "testcode"})
+	assert.NotNil(t, err)
+}
+
+func TestClient_GetUserByUnionID(t *testing.T) {
+	user, _, err := DingClient.GetUserByUnionID(ctx, &RequestGetByUnionID{UnionID: testUser.Result.UnionID})
+	assert.Nil(t, err)
+	assert.Equal(t, user.UserID, testUser.Result.UserID)
+}
+
 func TestClient_GetDepartment(t *testing.T) {
 	deptInfo, _, err := DingClient.GetDepartment(ctx, &RequestDepartmentInfo{DeptId: "155387235"})
 	assert.Nil(t, err)
